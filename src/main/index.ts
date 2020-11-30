32c4ee7e2a6bb5e657a693dd53c3b7aaaf5430a4
LukasWass <dev@lukaswass.se>
2020-11-30 11:11:24 +0100

    Added function to build top menu

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow, ipcMain, Menu } from "electron";
 import * as path from "path";
 import { exec } from "child_process";
 
@@ -6,93 +6,166 @@ declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 //Handle creating/removing shortcuts on Windows when installing/uninstalling.
 const HandleStartupEvent = () => {
-  if (require("electron-squirrel-startup")) {
-    //eslint-disable-line global-require
-    if (process.platform !== "win32") {
-      app.quit();
-      return false;
-    }
+	if (require("electron-squirrel-startup")) {
+		//eslint-disable-line global-require
+		if (process.platform !== "win32") {
+			app.quit();
+			return false;
+		}
 
-    const squirrelCommand = process.argv[1];
-    switch (squirrelCommand) {
-      case "--squirrel-install":
-      case "--squirrel-updated": {
-        const target = path.basename(process.execPath);
-        const updateDotExe = path.resolve(
-          path.dirname(process.execPath),
-          "..",
-          "update.exe"
-        );
-        const createShortcuts = `${updateDotExe} --createShortcut=${target} --shortcut-locations=Desktop,StartMenu`;
-        exec(createShortcuts);
+		const squirrelCommand = process.argv[1];
+		switch (squirrelCommand) {
+			case "--squirrel-install":
+			case "--squirrel-updated": {
+				const target = path.basename(process.execPath);
+				const updateDotExe = path.resolve(path.dirname(process.execPath), "..", "update.exe");
+				const createShortcuts = `${updateDotExe} --createShortcut=${target} --shortcut-locations=Desktop,StartMenu`;
+				exec(createShortcuts);
 
-        app.quit();
-        return true;
-      }
-      case "--squirrel-uninstall": {
-        const target = path.basename(process.execPath);
-        const updateDotExe = path.resolve(
-          path.dirname(process.execPath),
-          ".." + "update.exe"
-        );
-        const createShortcuts = `${updateDotExe} --removeShortcut=${target}`;
-        exec(createShortcuts);
+				app.quit();
+				return true;
+			}
+			case "--squirrel-uninstall": {
+				const target = path.basename(process.execPath);
+				const updateDotExe = path.resolve(path.dirname(process.execPath), ".." + "update.exe");
+				const createShortcuts = `${updateDotExe} --removeShortcut=${target}`;
+				exec(createShortcuts);
 
-        //Add code to delete app data
-        /*
+				//Add code to delete app data
+				/*
 					CODE
 				*/
 
-        app.quit();
-        return true;
-      }
-    }
-  }
+				app.quit();
+				return true;
+			}
+		}
+	}
 };
 
 HandleStartupEvent();
 
 let MainWindow: BrowserWindow = null;
 
-const CreateWindow = (): void => {
-  if (MainWindow === null) {
-    MainWindow = new BrowserWindow({
-      width: 1200,
-      height: 900,
-      webPreferences: {
-        nodeIntegration: true,
-        webSecurity: true,
-      },
-    });
+const CreateEventListners = () => {
+	ipcMain.on("AppCallingMainEvent", async (event, data: any) => {
+		console.log("AppCallingMainEvent data:", data);
 
-    MainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY); //Load index.html
-
-    MainWindow.webContents.openDevTools(); //Open dev tools
-  }
+		if (MainWindow !== null)
+			MainWindow.webContents.send("MainCallingAppEvent", "This is an event response");
+	});
 };
 
-const CreateEventListners = () => {
-  ipcMain.on("AppCallingMainEvent", async (event, data: any) => {
-    console.log("AppCallingMainEvent data:", data);
+const CreateWindow = (): void => {
+	if (MainWindow === null) {
+		MainWindow = new BrowserWindow({
+			width: 1200,
+			height: 900,
+			webPreferences: {
+				nodeIntegration: true,
+				webSecurity: true,
+			},
+		});
 
-    if (MainWindow !== null)
-      MainWindow.webContents.send(
-        "MainCallingAppEvent",
-        "This is an event response"
-      );
-  });
+		MainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY); //Load index.html
+
+		MainWindow.webContents.openDevTools(); //Open dev tools
+	}
+};
+
+const CreateMenu = () => {
+	const isMac = process.platform === "darwin";
+
+	//Menu item docs: https://www.electronjs.org/docs/api/menu-item
+	const template = [
+		...(isMac ? [{
+			label: app.name,
+			submenu: [
+				{ role: "about" },
+				{ type: "separator" },
+				{ role: "services" },
+				{ type: "separator" },
+				{ role: "hide" },
+				{ role: "hideothers" },
+				{ role: "unhide" },
+				{ type: "separator" },
+				{ role: "quit" }
+			]
+		}] : []),
+		{
+			label: "File",
+			submenu: [
+				isMac ? { role: "close" } : { role: "quit" }
+			]
+		},
+		{
+			label: "Edit",
+			submenu: [
+				{ role: "undo" },
+				{ role: "redo" },
+				{ type: "separator" },
+				{ role: "cut" },
+				{ role: "copy" },
+				{ role: "paste" },
+				{ type: "separator" },
+				{
+					label: "Custom button",
+					click: async () => {
+						console.log("Custom button");
+					}
+				}
+			]
+		},
+		{
+			label: "Window",
+			submenu: [
+				{ role: "minimize" },
+				{ role: "zoom" },
+				...(isMac
+					?
+					[
+						{ type: "separator" },
+						{ role: "front" },
+						{ type: "separator" },
+						{ role: "window" }
+					]
+					:
+					[
+						{ role: "close" }
+					]
+				)
+			]
+		},
+		{
+			role: "help",
+			submenu: [
+				{
+					label: "Learn More",
+					click: async () => {
+						const { shell } = require("electron")
+						await shell.openExternal("https://electronjs.org")
+					}
+				}
+			]
+		}
+	] as Electron.MenuItemConstructorOptions[];
+
+	const NewMenu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(NewMenu);
 };
 
 app.on("ready", () => {
-  CreateEventListners();
+	CreateEventListners();
 
-  CreateWindow();
+	CreateMenu();
+
+	CreateWindow();
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 /*
